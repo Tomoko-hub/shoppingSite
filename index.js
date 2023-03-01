cd1d7b24e575b3c021ccd2f0a946f62eb37d30d4
Tomoko-hub <haisaitomoco@gmail.com>
2023-03-01 10:21:50 +0200

    edit auth.js but token still work hard coding

--- index.js
+++ index.js
@@ -43,10 +43,10 @@ app.get("/", async(req, res)=> {
 })
 
 // Read Single Item
-app.get("/item/:_id", async(req, res)=> {
+app.get("/item/:id", async(req, res)=> {
     try {
         await connectDB()
-        const singleItem = await ItemModel.findById(req.params._id)
+        const singleItem = await ItemModel.findById(req.params.id)
         return res.status(200).json({
             message: "success read SINGLE items!",
             singleItem: singleItem
@@ -127,7 +127,7 @@ app.post("/user/login", async(req, res) => {
             if (req.body.password === savedUseData.password) {
                 const payload = {email: req.body.email}
                 const token = jwt.sign(payload, secret_key, { expiresIn: "23h" })
-                //console.log(token)
+                console.log(token)
                 return res.status(200).json({message: "Login success!", token: token})
             } else {
                 return res.status(400).json({message: "Please put correct password."})
--- utils/auth.js
+++ utils/auth.js
@@ -3,13 +3,16 @@ const  secret_key = "tomokon-mern-market"
 
 const auth = async(req, res, next) => {
     if(req.method === "GET"){
-        return next()
+        //return next()
+        return handler(req, res)
     }
 
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXIyQGdtYWlsLmNvbSIsImlhdCI6MTY3NzE2OTY1OSwiZXhwIjoxNjc3MjUyNDU5fQ.w5B9WUJxWSA8jMMueW5rvbyjUASJPKqDLuoNzq59A7o"
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImVtYWlsMUBnbWFpbC5jb20iLCJpYXQiOjE2Nzc2NTY5ODMsImV4cCI6MTY3NzczOTc4M30.ISkzZig6iXaGRjEw5HVbiJz5e9CmEv64wEjYpz7PNkY"
     //await req.headers.authorization.split("")[1]
+    //const token = await req.headers.authorization.split("")[1]
     if (!token){
-        return res.status(400).json({message: "You need to login again."})
+        //return res.status(400).json({message: "You need to login again."})
+        return res.status(401).json({message: "You don't have token."})
     }
     try{
         const decoded = jwt.verify(token, secret_key)
